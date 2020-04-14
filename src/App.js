5b351afec496f450e4be7b0f888b7789051c80e3
Lucian Dias <lucian.kl@gmail.com>
2020-04-14 01:39:36 -0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,45 +12,60 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const response = await api.get('repositories');
+      setRepositories(response.data);
+    }
+    loadRepositories();
+  }, []);
+
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    await api.post(`repositories/${id}/like`);   
+      
+    setRepositories(repositories.map(repo => 
+      repo.id === id ? {...repo, likes: repo.likes + 1} : repo));
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+      <FlatList 
+          data={repositories}
+          keyExtractor={repo => String(repo.id)}
+          renderItem={({ item: repository }) => 
+          <View style={styles.repositoryContainer}>
+            <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
+            <View style={styles.techsContainer}>
+              {repository.techs.map(tech => (
+                <Text key={tech} style={styles.tech}>
+                  {tech}
+                </Text>
+              ))}
+            </View>
+
+            <View style={styles.likesContainer}>
+              <Text
+                style={styles.likeText}                
+                testID={`repository-likes-${repository.id}`}>
+                {repository.likes} curtidas
+              </Text>
+            </View>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleLikeRepository(repository.id)}              
+              testID={`like-button-${repository.id}`}>
+              <Text style={styles.buttonText}>Curtir</Text>
+            </TouchableOpacity>
           </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+          }
+        />
       </SafeAreaView>
     </>
   );
@@ -65,6 +81,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
+    borderRadius: 4
   },
   repository: {
     fontSize: 32,
@@ -82,6 +99,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    borderRadius: 4
   },
   likesContainer: {
     marginTop: 15,
@@ -91,10 +109,10 @@ const styles = StyleSheet.create({
   likeText: {
     fontSize: 14,
     fontWeight: "bold",
-    marginRight: 10,
+    marginRight: 10
   },
-  button: {
-    marginTop: 10,
+  button: {    
+    marginTop: 10
   },
   buttonText: {
     fontSize: 14,
@@ -102,6 +120,7 @@ const styles = StyleSheet.create({
     marginRight: 10,
     color: "#fff",
     backgroundColor: "#7159c1",
-    padding: 15,
+    padding: 15,    
+    textAlign: "center",
   },
 });
